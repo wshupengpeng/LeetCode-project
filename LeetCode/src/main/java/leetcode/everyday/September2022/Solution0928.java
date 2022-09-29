83b1dcc91682d7bd310a340d8dafc5be5a946453
hpp <m18602830302@163.com>
2022-09-29 22:19:24 +0800

    提交0928和0929两天leetCode算法题，因为28号加班，因此29号提交两份

---
+++ LeetCode/src/main/java/leetcode/everyday/September2022/Solution0928.java
@@ -0,0 +1,76 @@
+package leetcode.everyday.September2022;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/**
+ * @creater hpp
+ * @Date 2022/9/29-21:21
+ * @description: 面试题 17.09. 第 k 个数
+ *
+ * 有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。注意，不是必须有这些素因子，而是必须不包含其他的素因子。例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
+ * 示例 1:
+ *
+ * 输入: k = 5
+ *
+ * 输出: 9
+ */
+public class Solution0928 {
+    /**
+     * 解题思路1：
+     * 分析题目可知，所谓素因子为3,5,7说明当前数的k可以分解为3,5,7的倍数，而不可以被其他数而分解。
+     * 并且按照题意可知，当前数需要被排序。
+     *
+     * 通过最小堆方式进行排序，即当前排序数为x,下一个排序数为 3x 5x 7x 中的最小值，每次一轮，直到结束当前k轮，即获取第一个数为要求的数
+     * @param k
+     * @return
+     */
+    public int getKthMagicNumber(int k) {
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+        minHeap.add(1L);
+        Set<Long> distinct = new HashSet<>();
+        int[] baseNum = new int[]{3, 5, 7};
+        for (int i = 2; i <= k; i++) {
+            long poll = minHeap.poll();
+            for (int num : baseNum) {
+                long res = poll * num;
+                if (distinct.add(res)) {
+                    minHeap.offer(res);
+                }
+            }
+        }
+        return minHeap.poll().intValue();
+    }
+
+    /**
+     * 算法二： 改进方案，因为算法1采用最小堆，而每次遍历都会加3*x，5*x，7*x，会添加多余的数到队列中，因此
+     * 可以通过动态规划进行优化，即每次取当前最小的值为下一次需要计算的开始，因此有当前数x，其下一个数据为 Min(3*x,5*x,7*x)，
+     * 当选中下一个数时，其对应的素数坐标+1，即参与下一次计算。
+     */
+
+    public int getKthMagicNumber1(int k) {
+        int s1 = 0, s2 = 0, s3 = 0;
+        int res[] = new int[k];
+        res[0] = 1;
+        for(int i = 1; i < k; i++){
+            res[i] = Math.min(3 * res[s1], Math.min(5 * res[s2], 7 * res[s3]));
+            if (res[i] == 3 * res[s1]) {
+               s1++;
+            }
+            if (res[i] == 5 * res[s2]) {
+                s2++;
+            }
+            if (res[i] == 7 * res[s3]) {
+                s3++;
+            }
+        }
+        return res[k-1];
+    }
+
+    public static void main(String[] args) {
+        Solution0928 solution0928 = new Solution0928();
+        int kthMagicNumber1 = solution0928.getKthMagicNumber1(10);
+        System.out.println(kthMagicNumber1);
+    }
+}
---
+++ LeetCode/src/main/java/leetcode/everyday/September2022/Solution0929.java
@@ -0,0 +1,70 @@
+package leetcode.everyday.September2022;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @creater hpp
+ * @Date 2022/9/29-20:58
+ * @description:   面试题 01.09. 字符串轮转
+ * 字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+ *
+ * 示例1:
+ *
+ *  输入：s1 = "waterbottle", s2 = "erbottlewat"
+ *  输出：True
+ * 示例2:
+ *
+ *  输入：s1 = "aa", s2 = "aba"
+ *  输出：False
+ */
+public class Solution0929 {
+    /**
+     *  解题1方式：
+     *  假设s1重排后的字符串为s2，那么有以j下标字段按照i坐标旋转， 有s1[(i+j) % s1.length] = s2[j]
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean isFlipedString(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        if (s1.length() == 0) {
+            return true;
+        }
+        int n = s1.length();
+        boolean res = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt((i + j) % n) != s2.charAt(j)) {
+                    res = false;
+                    break;
+                }
+            }
+            if (res) {
+                return true;
+            }
+            res = true;
+        }
+        return false;
+    }
+
+
+    /**
+     *  解题2方式：
+     *  假设s1以i旋转后为s2，那么将s1以i坐标分为两块，分别为ss1和ss2,
+     *  即 ss1 + ss2 = s1， ss2 + ss1 = s2
+     *  s1 + s1 = ss1 + ss2 + ss1 + ss2
+     *  s1 + s2 = ss1 + s2 + ss2
+     *  那么 s1+s1后的字符串必包含s2
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean isFlipedString1(String s1, String s2) {
+        if(s1.length() != s2.length()) return false;
+        if(s1.length() == 0) return true;
+        return (s1+s1).contains(s2);
+    }
+}
